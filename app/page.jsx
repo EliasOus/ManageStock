387077ce82e8da42a0f65149bd2f31581b2df5ef
Anisse <AnisseBen@users.noreply.github.com>
2025-02-12 10:37:09 -0500

    commenter mon test de la carte sur page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,13 @@ import srcImage1 from "@/public/chart-img.png"
 import srcImage2 from "@/public/identifiez-img.jpg"
 export default function Home() {
   return <>
+    {/* 
+    test sur carte Anisse
     <Carte
       titre="Identifiez rapidement vos produits les plus performants"
       paragraphe="Grace a ManageStock, analysez facilement les donnees de vente et isolez vos produits qui generent le plus de revenus. Prenez des decisions eclairees pour optimiser votre strategie et maximiser vos profits."
       srcImage1= {srcImage1}
       srcImage2= {srcImage2}
-    />
+    /> */}
   </>;
 }
