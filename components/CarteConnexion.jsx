e97eaea2d4806a66c7a5d5c07f9a8b8479540676
Anisse <AnisseBen@users.noreply.github.com>
2025-02-12 12:48:41 -0500

    ajustement de CarteConnexion.jsx et son css

--- components/CarteConnexion.jsx
+++ components/CarteConnexion.jsx
@@ -1,10 +1,33 @@
-import styles from "./carteConnexion.module.css"
+import styles from "./CarteConnexion.module.css"
 import Image from "next/image"
 
 import logo from "@/public/Logo-White.png"
 
 export default function CarteConnexion() {
     return <>
-
+        <section className={styles.maSection}>
+            <div className={styles.logo}>
+                <Image src={logo} alt="logo ManageStock" className={styles.logoImage}/>
+            </div>
+            <div className={styles.liens}>
+                <div><a href="#">Connexion</a></div>
+                <div><a href="#">Creer un Compte</a></div>
+            </div>
+            <div className={styles.zoneText}>
+                <input type="text" placeholder="Nom d'entreprise" />
+            </div>
+            <div className={styles.zoneText}>
+                <input type="text" placeholder="Email" />
+            </div>
+            <div className={styles.zoneText}>
+                <input type="text" placeholder="Nom d'utilisateur" />
+            </div>
+            <div className={styles.zoneText}>
+                <input type="text" placeholder="Mot de Passe" />
+            </div>
+            <div>
+                {/* Boutton lady */}
+            </div>
+        </section>
     </>
 }
