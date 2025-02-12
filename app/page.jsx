15044c9e0aba9a7015d0b32c609dfdeb73c6aafd
Anisse <AnisseBen@users.noreply.github.com>
2025-02-12 10:34:11 -0500

    finaliser Carte.jsx et son css + test sur page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
 import Carte from "@/components/Carte";
 import styles from "./page.module.css";
 
+import srcImage1 from "@/public/chart-img.png"
+import srcImage2 from "@/public/identifiez-img.jpg"
 export default function Home() {
   return <>
-  <Carte/>
+    <Carte
+      titre="Identifiez rapidement vos produits les plus performants"
+      paragraphe="Grace a ManageStock, analysez facilement les donnees de vente et isolez vos produits qui generent le plus de revenus. Prenez des decisions eclairees pour optimiser votre strategie et maximiser vos profits."
+      srcImage1= {srcImage1}
+      srcImage2= {srcImage2}
+    />
   </>;
 }
