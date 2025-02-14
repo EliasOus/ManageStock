513b8a995dd4960e79df2d2798e924a95209c1f7
Sreemer1 <jergi9@hotmail.com>
2025-02-14 09:01:53 -0500

    Modification en css
    jai changer carte avantage et section entreprise en css. no react et trailwind

--- components/CarteAvantage.jsx
+++ components/CarteAvantage.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import styles from './CarteAvantage.module.css';
 
 const CarteAvantage = ({ imageSrc, title, description }) => {
   return (
-    <div className="flex flex-col items-center text-center p-6 bg-white shadow-lg rounded-lg">
+    <div className={styles.card}>
       <Image src={imageSrc} alt={title} width={60} height={60} />
-      <h3 className="text-lg font-semibold mt-4">{title}</h3>
-      <p className="text-gray-600 mt-2">{description}</p>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
     </div>
   );
 };
