6b92dfeda3ecf4598e4fb09c3a1e938454e21e0d
Sreemer1 <jergi9@hotmail.com>
2025-02-26 12:13:17 -0500

    Update CarteAvantage.jsx
    Correctif

--- components/CarteAvantage.jsx
+++ components/CarteAvantage.jsx
@@ -4,7 +4,7 @@ import styles from './CarteAvantage.module.css';
 const CarteAvantage = ({ imageSrc, title, description }) => {
   return (
     <div className={styles.card}>
-      <Image src={imageSrc} alt={title} ajoute quality={100} />
+      <Image src={imageSrc} alt={title} quality={100} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
     </div>
