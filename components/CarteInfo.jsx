5b6bd242312a1e467a46dbc7f982cdbd2b44bdc5
Anisse <AnisseBen@users.noreply.github.com>
2025-03-09 21:53:14 -0400

    re-ajustement de CarteInfo et son css

--- components/CarteInfo.jsx
+++ components/CarteInfo.jsx
@@ -5,12 +5,10 @@ import styles from "./CarteInfo.module.css"
 export default function CarteInfo({ chiffre, titre }) {
     return (
         <>
-            <section className={styles.section}>
-                <div className={styles.maDiv}>
-                    <p className={styles.chiffre} >{chiffre}</p>
-                    <p className={styles.titre}>{titre}</p>
-                </div>
-            </section>
+            <div className={styles.maDiv}>
+                <p className={styles.chiffre} >{chiffre}</p>
+                <p className={styles.titre}>{titre}</p>
+            </div>
         </>
     )
 }
