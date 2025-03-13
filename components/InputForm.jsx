e2fd8b33af9b24da8647b1b942127cae1bf12c19
Phanuelle <phanuellienou@gmail.com>
2025-03-13 09:46:44 -0400

    creation de la page inventaire

--- components/InputForm.jsx
+++ components/InputForm.jsx
@@ -4,64 +4,65 @@ import { useState } from "react";
 import Button from "./Button";
 import styles from "./InputForm.module.css";
 
-export default function InputForm({ className, inputFields }) {
-  const [inputValues, setInputValues] = useState(
-    Object.fromEntries(inputFields.map(field => [field.name, ""]))
-  );
+export default function InputForm({ className, inputFields, onClose }) {
+ const [inputValues, setInputValues] = useState(
+  Object.fromEntries(inputFields.map((field) => [field.name, ""]))
+ );
 
-  const handleInputChange = (name, value) => {
-    setInputValues(prev => ({
-      ...prev,
-      [name]: value
-    }));
-  };
+ const handleInputChange = (name, value) => {
+  setInputValues((prev) => ({
+   ...prev,
+   [name]: value,
+  }));
+ };
 
-  const handleSubmit = (e) => {
+ const handleSubmit = (e) => {
   e.preventDefault();
 
   // Loop through all input fields to check if any field is empty
   for (const [key, value] of Object.entries(inputValues)) {
-    if (!value.trim()) {
-      alert(`Please fill in the ${key} field.`);
-      return; // Stop submission if a field is empty
-    }
+   if (!value.trim()) {
+    alert(`Please fill in the ${key} field.`);
+    return; // Stop submission if a field is empty
+   }
   }
 
   // If all fields are filled, proceed with the submission
   console.log("Form values:", inputValues);
-};
+ };
 
-
-  return (
-    <form onSubmit={handleSubmit} className={`${styles.form} ${className}`}>
-      {inputFields.map((field, index) => (
-        <div key={index}>
-          {field.name === "description" ? (
-            // Render a textarea for the description input
-            <textarea
-              name={field.name}
-              value={inputValues[field.name]}
-              onChange={(e) => handleInputChange(field.name, e.target.value)}
-              placeholder={field.placeholder}
-              className={`${styles.input} ${styles.textarea}`} // Apply additional CSS for larger input
-            />
-          ) : (
-            // Render a normal input field
-            <input
-              type="text"
-              name={field.name}
-              value={inputValues[field.name]}
-              onChange={(e) => handleInputChange(field.name, e.target.value)}
-              placeholder={field.placeholder}
-              className={styles.input}
-            />
-          )}
-        </div>
-      ))}
-      <div className={styles.buttonContainer}>
-        <Button texte="Enregistrer" type="submit" active={true} />
-        <Button texte="Annuler" type="button" active={true} />
-      </div>
-    </form>
-  );
+ return (
+  <form onSubmit={handleSubmit} className={`${styles.form} ${className}`}>
+   {inputFields.map((field, index) => (
+    <div key={index}>
+     {field.name === "description" ? (
+      // Render a textarea for the description input
+      <textarea
+       name={field.name}
+       value={inputValues[field.name]}
+       onChange={(e) => handleInputChange(field.name, e.target.value)}
+       placeholder={field.placeholder}
+       className={`${styles.input} ${styles.textarea}`} // Apply additional CSS for larger input
+      />
+     ) : (
+      // Render a normal input field
+      <input
+       type="text"
+       name={field.name}
+       value={inputValues[field.name]}
+       onChange={(e) => handleInputChange(field.name, e.target.value)}
+       placeholder={field.placeholder}
+       className={styles.input}
+      />
+     )}
+    </div>
+   ))}
+   <div className={styles.buttonContainer}>
+    <Button texte="Enregistrer" type="submit" active={true} />
+    <div onClick={onClose}>
+     <Button texte="Annuler" type="button" active={true} />
+    </div>
+   </div>
+  </form>
+ );
 }
