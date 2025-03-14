9238a5690e879e268d44b37768ae8061025270a0
Ousameur Elias <ousameurelias@gmail.com>
2025-03-13 20:21:15 -0400

    creer la page d'accueil

--- app/(manageStock)/page.jsx
+++ app/(manageStock)/page.jsx
@@ -1,5 +1,11 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import banner from "@/public/banner.png";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className={styles.sectionBanner}>
+      <Image src={banner} alt={"banner de la page d'accueil"} />
+    </div>
+  );
 }
