9342e4a59a4d3145d1f9342f0062901496a67c99
Anisse <AnisseBen@users.noreply.github.com>
2025-03-18 21:20:33 -0400

    creation page recherche et son css

--- app/(dashboard)/dashboard/recherche/page.jsx
+++ app/(dashboard)/dashboard/recherche/page.jsx
@@ -7,23 +7,25 @@ import datas from "@/data/datas.json";
 export default function Recherche() {
     return (
         <>
-            <div className={styles.divRecherche}>
-                <Recherche1 />
+            <div className={styles.contenaire}>
+                <div className={styles.divRecherche}>
+                    <Recherche1 />
+                </div>
+                <div className={styles.infoBloc}>
+                    <InfoBloc
+                        defaultTitle={"Recherche Article"}
+                        defaultHeaders={[
+                            "Upe/Sku",
+                            "Nom d’article",
+                            "Fournisseur",
+                            "Quantité",
+                            "Prix",
+                        ]}
+                        data={datas.produitsRupture}
+                    />
+                </div>
             </div>
-            <div className={styles.infoBloc}>
-                <InfoBloc
-                    defaultTitle={"Recherche Article"}
-                    defaultHeaders={[
-                        "Upe/Sku",
-                        "Nom d’article",
-                        "Fournisseur",
-                        "Quantité",
-                        "Prix",
-                    ]}
-                    data={datas.produitsRupture}
-                />
 
-            </div>
         </>
     )
 }
